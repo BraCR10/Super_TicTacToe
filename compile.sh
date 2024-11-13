5c183119debde0bb0cab6e641ce31d2acbf19aaf
BraCR10 <brianramirez01arias@gmail.com>
2024-11-13 17:45:24 -0600

    New organization

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 
 EXEC_NAME="super_tic_tac_toe"
 
-SRC_FILES="src/main/main.c src/interface/interface.c"
+SRC_FILES="src/main.c src/interface/interface.c"
 
-gcc -o $EXEC_NAME $SRC_FILES $(pkg-config --cflags --libs gtk+-3.0)
+gcc -I include -o $EXEC_NAME $SRC_FILES $(pkg-config --cflags --libs gtk+-3.0)
 
 
 
