25f2986225bfe5b9d350c13b7dbdca39262135ff
BraCR10 <brianramirez01arias@gmail.com>
2024-11-13 16:09:34 -0600

    Update

--- interface.c
+++ interface.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
 
 void interface_call(int argc, char *argv){
-    printf("Número de argumentos:\n");
-    for (int i = 0; i < 2; i++) {
-        printf("Argumento \n");
-    }
 }
