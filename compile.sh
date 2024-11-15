6f10268e6dcd2355deda7507aa57c36137059520
BraCR10 <brianramirez01arias@gmail.com>
2024-11-15 17:33:10 -0600

    New script

--- compile.sh
+++ compile.sh
@@ -4,19 +4,27 @@
 # sudo apt install libgdk-pixbuf2.0-dev:i386 libglib2.0-dev:i386 libpango1.0-dev:i386 libcairo2-dev:i386
 EXEC_NAME="super_tic_tac_toe.exe"
 
-C_FILES="src/main.c 
-        src/interface/interface.c 
-        src/c_functions/buttons_actions.c 
-        src/c_functions/record_movement.c
-        src/c_functions/check_button.c"
+nasm -f elf32 src/asm_functions/search_position.asm -o asm_objects/search_position.o
+nasm -f elf32 src/asm_functions/memory_setup.asm -o asm_objects/memory_setup.o
 
-nasm -f elf32 src/asm_functions/search_position.asm
-nasm -f elf32 src/asm_functions/memory_setup.asm
-            
+NASM_OBJECTS="asm_objects/search_position.o
+                asm_objects/memory_setup.o"
 
-NASM_OBJECTS="src/asm_functions/search_position.o
-              src/asm_functions/memory_setup.o"
+gcc  -m32 -I include -c src/main.c  -o c_objects/main.o 
+gcc -m32 -I include -c src/c_functions/record_movement.c  -o c_objects/record_movement.o
 
-gcc -m32 -I include -o $EXEC_NAME $C_FILES $NASM_OBJECTS $(pkg-config --cflags --libs gtk+-3.0)
+C_OBJECTS="c_objects/main.o 
+        c_objects/interface.o
+        c_objects/buttons_actions.o
+        c_objects/record_movement.o
+        c_objects/check_button.o"
+        
 
-./$EXEC_NAME
+gcc -c -I include -o c_objects/interface.o src/interface/interface.c $(pkg-config --cflags --libs gtk+-3.0)
+gcc -c -I include -o c_objects/buttons_actions.o src/c_functions/buttons_actions.c $(pkg-config --cflags --libs gtk+-3.0)
+gcc -c -I include -o c_objects/check_button.o src/c_functions/check_button.c $(pkg-config --cflags --libs gtk+-3.0)
+
+gcc  -m32  -o $EXEC_NAME $C_OBJECTS $NASM_OBJECTS  $(pkg-config --cflags --libs gtk+-3.0) -rdynamic
+
+
+#./$EXEC_NAME
